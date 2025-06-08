a5e54060d76fa70ab5ec92dc644980390e3a3d74
Daniel Dallagnol <danieldallagnol9@gmail.com>
2025-06-08 19:27:00 -0300

    fix- dados.tsx pra ficar bonito o loading

--- src/Pages/Dados.tsx
+++ src/Pages/Dados.tsx
@@ -4,45 +4,51 @@ import { useParams } from "react-router-dom";
 import Titulo from '../Components/Titulo'
 
 type PostaDados = {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
 };
 
 export default function PageDados() {
-  const { id } = useParams<{ id: string }>();
-  const [post, setPost] = useState<PostaDados | null>(null);
+    const { id } = useParams<{ id: string }>();
+    const [post, setPost] = useState<PostaDados | null>(null);
 
-  useEffect(() => {
-    if (id) {
-      fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-        .then((res) => res.json())
-        .then((data) => setPost(data));
+    useEffect(() => {
+        if (id) {
+            fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+                .then((res) => res.json())
+                .then((data) => setPost(data));
+        }
+    }, [id]);
+
+
+
+    {/*tem que colocar senão ele da erro que pode ser null*/ }
+    if (!post) {
+        return (
+            <div>
+
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 5, alignContent: 'center', alignItems: 'center' }}>
+                    <Titulo texto="Carregando detalhes do post..."></Titulo>
+                </Box>
+            </div>)
     }
-  }, [id]);
-
-  
-  
-  {/*tem que colocar senão ele da erro que pode ser null*/}
-  if (!post) {
-    return <div>Carregando detalhes do post...</div>;
-  }
-  
 
 
-  return (
-    <div> 
-   
-    <Box sx={{display:'flex', flexDirection:'column', gap:5, alignContent:'center', alignItems:'center'}}>
-      <Titulo texto={post.title}></Titulo>
 
-      <Typography variant="subtitle2">Post ID: {post.id}</Typography>
-      <Typography variant="subtitle2">User ID: {post.userId}</Typography>
-      <Typography variant="body1" sx={{ whiteSpace: "pre-line" }}>
-        {post.body}
-      </Typography>
-    </Box>
-    </div>   
-  );
+    return (
+        <div>
+
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 5, alignContent: 'center', alignItems: 'center' }}>
+                <Titulo texto={post.title}></Titulo>
+
+                <Typography variant="subtitle2">Post ID: {post.id}</Typography>
+                <Typography variant="subtitle2">User ID: {post.userId}</Typography>
+                <Typography variant="body1" sx={{ whiteSpace: "pre-line" }}>
+                    {post.body}
+                </Typography>
+            </Box>
+        </div>
+    );
 }

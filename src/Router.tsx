a7c93a6ebca354c50ab676729a144b38f05b0158
Daniel Dallagnol <danieldallagnol9@gmail.com>
2025-06-08 17:05:24 -0300

    fix- fix release_husky

---
+++ src/Router.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import App from './App'
+
+
+
+
+export function ExportaRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+
+        <Route path="*" element={<h2 color='black'>404 Página não encontrada</h2>} />
+      </Routes>
+    </Router>
+  )
+}
+

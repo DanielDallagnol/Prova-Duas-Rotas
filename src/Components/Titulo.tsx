46c39856fc649280078e260fe6b6efff6954d63e
Daniel Dallagnol <danieldallagnol9@gmail.com>
2025-06-09 00:58:55 -0300

    fix- arrumar a fonte do titulo

--- src/Components/Titulo.tsx
+++ src/Components/Titulo.tsx
@@ -2,19 +2,20 @@ import { Typography } from "@mui/material";
 
 interface TitleProps {
   texto: string;
-
+  fontWeight?: string | number; // pode ser 'bold', 'normal', 400, 700 etc.
 }
 
-export default function ComponentTitle({ texto }: TitleProps) {
+export default function ComponentTitle({ texto, fontWeight = 'bold' }: TitleProps) {
   return (
     <div>
-      
-      <Typography sx={{}} variant="h3" component="h2">
+      <Typography
+        sx={{ color: 'white', fontWeight, fontFamily: 'Poppins, sans-serif'}}
+        variant="h3"
+        component="h2"
+      >
         {texto}
       </Typography>
-
-      
     </div>
-
   );
 }
+
--- src/Pages/Post.tsx
+++ src/Pages/Post.tsx
@@ -7,8 +7,11 @@ export default function PagePost() {
     return (
         <div>
             <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3, alignContent: 'center', alignItems: 'center', margin: "1%" }}>
+                <Box sx={{display: 'flex', flexDirection: 'row',gap:2}}>
+                    <Titulo texto="LISTA " fontWeight="bold" ></Titulo>
+                    <Titulo texto="de POSTS" fontWeight="lighter"></Titulo>
+                </Box>
 
-                <Titulo texto="Lista de Posts"></Titulo>
                 <Card sx={{ display: 'flex', flexDirection: 'column', alignContent: 'center', alignItems: 'center', margin: "1%" }}>
 
                     <Lista></Lista>

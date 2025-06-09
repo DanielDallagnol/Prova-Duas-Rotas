f00e0e4c132ec7c929d892dd2ba053b3465d484c
Daniel Dallagnol <danieldallagnol9@gmail.com>
2025-06-09 02:02:57 -0300

    fix- mudar a cor do alert

--- src/Components/Alert.tsx
+++ src/Components/Alert.tsx
@@ -1,20 +1,18 @@
-import { Alert,  LinearProgress } from "@mui/material";
+import { Alert, LinearProgress } from "@mui/material";
 
 interface ButtonsProps {
-    
-    texto: string;
 
-  }
-  
-  export default function ComponentAlert({texto }: ButtonsProps) {
-    return (
-        <div>
+  texto: string;
 
-            
-        <Alert variant="filled" severity="warning">
+}
+
+export default function ComponentAlert({ texto }: ButtonsProps) {
+  return (
+    <div>
+      <Alert variant="filled" severity="warning" sx={{backgroundColor: "#F9C80E", color:'black'}}>
         {texto}
       </Alert>
-      <LinearProgress  color="warning" />
-      </div>
-    );
-  }
+      <LinearProgress color="warning" />
+    </div>
+  );
+}
